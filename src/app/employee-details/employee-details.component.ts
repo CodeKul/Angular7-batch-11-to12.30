4b076e9ca1f6c1ec61b0cf58395b8d0944d080d0
Kalyani Chute <melayer.kalyani@gmail.com>
2019-02-21 12:32:01 +0530

    reading router variable by using observable

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css']
 })
-export class EmployeeDetailsComponent implements OnInit {
-
-  constructor() { }
+export class EmployeeDetailsComponent implements OnInit ,OnDestroy{
+  abc: string;
+  sub :Subscription
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    //  this.usNm = this.activatedRoute.snapshot.params['usNm'];
+    this.activatedRoute.params.subscribe(newParam => {
+      this.abc = newParam['usNm']
+    })
   }
-
+  ngOnDestroy(){
+    this.sub.unsubscribe()
+  }
+  changeUsNm(chUsNm: string) {
+    console.log(chUsNm);
+    // this.usNm = chUsNm
+    this.router.navigate(['employeedetails', chUsNm])
+  }
 }
